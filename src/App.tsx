0aefc155aca8967e1912f1cc2c059ffb6fe64dbb
abdujs <abdu12.aau@gmail.com>
2025-12-29 11:55:23 +0300

    chore: initial frontend setup with React, TypeScript, Tailwind CSS v4

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <button className="bg-[var(--color-primary)] text-white px-[var(--spacing-base)] py-2 rounded">
+        Themed Button
+      </button>
+    </>
+  )
+}
+
+export default App
